2eefe2266bab6fb87017db944d88718ad439d428
AyMax77 <157603651+AyMax77@users.noreply.github.com>
2024-03-26 16:43:01 +0100

    Add files via upload

--- Employe.kt
+++ Employe.kt
@@ -1,11 +1,33 @@
+import java.time.LocalDate
+import java.time.LocalTime
 class Employe {
-    val Matricule = ""
-    val Nom = ""
-    val Prenom = ""
-    val AnneeEmbauche = 0
-    val Salaire =  0
+    var Matricule = ""
+    var Nom = ""
+    var Prenom = ""
+    var AnneeEmbauche = 0
+    var Salaire =  0.0
 
-    constructor(Matricule)
+    constructor(Matricule: String, Nom: String, Prenom: String, AnneeEmbauche: Int, Salaire: Double) {
+        this.Matricule = Matricule
+        this.Nom = Nom
+        this.Prenom = Prenom
+        this.AnneeEmbauche = AnneeEmbauche
+        this.Salaire = Salaire
+    }
+    fun anciennete(AnneeEmbauche: Int){
+        val anneeEnCours = LocalDate.now().year
+        anneeEnCours - this.AnneeEmbauche
+    }
+    fun AugmentationDuSalaire(anciennete : Double){
+        if (anciennete <= 5 ){
+            this.Salaire *= 1.02
+        } else if (anciennete <= 10){
+            this.Salaire *= 1.05
+        } else {
+            this.Salaire *= 1.10
+        }
+    }
 
+    
 
 }
